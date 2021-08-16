65026310f9505501387c66646989e1024982a3bb
Learn010 <geenbagger+github@gmail.com>
2021-08-16 22:47:07 +0200

    html added

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^2h*&@$o47xv&babu3iogr1!9xr%^%#zke^(7f0&cq!sd*4^_e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
